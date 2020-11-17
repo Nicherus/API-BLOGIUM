8342c7fb6cc4fcfd8a000b2d17384defd6512587
Nicherus <joaonichev@gmail.com>
2020-11-17 02:03:52 -0300

    feat: implement base request paths

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,32 +1,29 @@
 import express from 'express';
 
 const app = express();
-app.use(express.json());
 
 app.post('/api/posts', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
 app.get('/api/posts', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
 app.get('/api/users/:id/posts', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
 app.get('/api/posts/:id', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
 app.put('/api/posts/:id', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
 app.delete('/api/posts/:id', (request, response) => {
-	return response.status(200).send('zap');
+	response.status(200).send('zap');
 });
 
-app.listen(3000, () => {
-	console.log('Server is listening on port 3000.');
-});
+app.listen(3000);
